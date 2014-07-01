55e541571c90e46c04d7daa19f9346f7e882ab5e
Eugene Sypachev <e.sypachev@eis.ru>
2014-07-01 16:21:55 +0400

    config string

--- src/main/scala/ru/eis/azurejay/Azurejay.scala
+++ src/main/scala/ru/eis/azurejay/Azurejay.scala
@@ -9,6 +9,21 @@ package ru.eis.azurejay
 /**
  * Plugin interface
  */
-trait Azurejay extends play.api.Plugin {
-  def test: Unit
+trait AzureJay extends play.api.Plugin {
+  val pluginName = "azurejay"
+}
+
+class AzureJayPlugin(app: play.api.Application) extends AzureJay {
+
+  private lazy val instance : Unit = {
+    val serviceName : String = app.configuration.getString("azurejay.service").getOrElse(
+      throw new RuntimeException("azurejay.service must be set in application.conf in order to use plugin"))
+    val tableName : String = app.configuration.getString("azurejay.table").getOrElse(
+      throw new RuntimeException("azurejay.table must be set in application.conf in order to use plugin"))
+  }
+
+  override lazy val enabled = {
+    !app.configuration.getString("azurejay").filter(_ == "disabled").isDefined
+  }
+
 }
